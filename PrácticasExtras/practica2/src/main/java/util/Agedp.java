c61b07525c50acc1b7579b0177d33813f943eaf8
DIR\ivan.c.contreras <ivan11926@hotmail.com>
2021-07-21 00:38:40 -0500

    Se agrego la practica 2 de las practicas extras

---
+++ PrácticasExtras/practica2/src/main/java/util/Agedp.java
@@ -0,0 +1,14 @@
+package util;
+
+public class Agedp {
+
+    /*Se tinene que crear un constructor vacio, que es el que llama
+    * la suvclase Kinder en su unico constructor
+    * */
+
+    Agedp(){  }
+
+    public Agedp(int x){
+
+    }
+}
---
+++ PrácticasExtras/practica2/src/main/java/util/Kinder.java
@@ -0,0 +1,12 @@
+package util;
+
+public class Kinder extends Agedp{
+
+   public Kinder(int x){
+       /*se tiene que hacer referencia al contructor de la clase padre con super
+       * por ende se debe crear un constructor vacio en la clase padre
+       * */
+
+       super();
+   }
+}
