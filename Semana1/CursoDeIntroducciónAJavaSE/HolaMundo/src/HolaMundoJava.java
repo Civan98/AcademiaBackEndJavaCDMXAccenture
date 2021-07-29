9dd1218cffb6f3b6ed91d1bfde3a57d2be795963
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-07-29 15:06:01 -0500

    Eliminación de guión bajo en nombre de directorios

--- Semana1/CursoDeIntroducciónAJavaSE/HolaMundo/src/HolaMundoJava.java
+++ Semana1/CursoDeIntroducciónAJavaSE/HolaMundo/src/HolaMundoJava.java
@@ -1,5 +1,5 @@
 public class HolaMundoJava {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         System.out.println("Hola Accenture");
         int x = 1;
         while (x <= 10) {
