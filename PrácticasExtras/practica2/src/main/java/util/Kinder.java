f6ddfbe05671d28b6a86207e6d74d9b0451b3387
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-07-29 14:51:23 -0500

    Se agrego la práctica 2 de las prácticas extras

--- PrácticasExtras/practica2/src/main/java/util/Kinder.java
+++ PrácticasExtras/practica2/src/main/java/util/Kinder.java
@@ -3,7 +3,7 @@ package util;
 public class Kinder extends Agedp{
 
    public Kinder(int x){
-       /*se tiene que hacer referencia al contructor de la clase padre con super
+       /*se tiene que hacer referencia al contructor de la clase padre con super 
        * por ende se debe crear un constructor vacio en la clase padre
        * */
 
