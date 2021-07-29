34f7173f9d27bdc9f6aa05e667c3cb584f8c4ea4
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-07-29 14:51:08 -0500

    Se agrego la práctica 2 de las prácticas extras

--- PrácticasExtras/practica2/src/main/java/util/Agedp.java
+++ PrácticasExtras/practica2/src/main/java/util/Agedp.java
@@ -5,7 +5,7 @@ public class Agedp {
     /*Se tiene que crear un constructor vacio, que es el que llama
     * la suvclase Kinder en su único constructor
     * */
-
+ 
     Agedp(){  }
 
     public Agedp(int x){
