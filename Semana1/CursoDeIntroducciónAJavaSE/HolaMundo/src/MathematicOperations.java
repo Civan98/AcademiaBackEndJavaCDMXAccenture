7bc0d4a8ffebee46c9829e31e0996f0dacc064c9
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-07-29 15:07:31 -0500

    Eliminación de guión bajo en nombre de directorios

--- Semana1/CursoDeIntroducciónAJavaSE/HolaMundo/src/MathematicOperations.java
+++ Semana1/CursoDeIntroducciónAJavaSE/HolaMundo/src/MathematicOperations.java
@@ -1,7 +1,7 @@
 public class MathematicOperations {
     public static void main(String[] args) {
         double x = 2.1;
-        double y = 3;
+        double y = 3; 
 
         //Devuelve un entero hacia arriba
         System.out.println(Math.ceil(x));
