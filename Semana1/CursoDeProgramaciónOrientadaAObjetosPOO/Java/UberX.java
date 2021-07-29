b0fec42b035b607f8fd5da7be4088d336e8dc5a4
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-07-29 15:18:30 -0500

    Eliminación de guión bajo en nombre de directorios

--- Semana1/CursoDeProgramaciónOrientadaAObjetosPOO/Java/UberX.java
+++ Semana1/CursoDeProgramaciónOrientadaAObjetosPOO/Java/UberX.java
@@ -1,7 +1,7 @@
 package Java;
 class UberX extends Car {
     String brand;
-    String model;
+    String model; 
 
     public UberX(String license, Account driver, String brand, String model){
         super(license, driver);
