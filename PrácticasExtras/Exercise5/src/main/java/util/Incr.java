7b9d3aeb79f808e5551ca00c05e01de0f8058475
DIR\ivan.c.contreras <ivan11926@hotmail.com>
2021-07-29 10:47:23 -0500

    se agregaron los ejercicios 4 y 5 de las practicas extras

---
+++ PrácticasExtras/Exercise5/src/main/java/util/Incr.java
@@ -0,0 +1,17 @@
+package util;
+
+public class Incr {
+    public static void main(String[] args) {
+        //Resultado 33
+        Integer x = 7;
+        int y = 2;
+
+        x *= x;
+        y *= y;
+        y *= y;
+        x -= y;
+
+        System.out.println(x);
+        //System.out.println(y);
+    }
+}
