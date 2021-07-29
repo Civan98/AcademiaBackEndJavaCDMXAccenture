338a5615fb8d93b22dd1f2a8785431571873fee0
Ivan Catañeda Contreras <52634607+Civan98@users.noreply.github.com>
2021-07-29 15:04:56 -0500

    Eliminación de guión bajo en nombre de directorios

--- Semana1/CursoDeIntroducciónAJavaSE/HolaMundo/src/ForLoop.java
+++ Semana1/CursoDeIntroducciónAJavaSE/HolaMundo/src/ForLoop.java
@@ -4,7 +4,7 @@ public class ForLoop {
         turnOnOffLight();
         for (int i = 1; i <= 10; i++) {
             printSOS();
-        }
+        } 
     }
     public static void printSOS(){
         System.out.println(". . . _ _ _ . . .");
