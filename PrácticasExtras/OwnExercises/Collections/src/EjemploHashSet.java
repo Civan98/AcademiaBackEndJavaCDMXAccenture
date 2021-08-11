4351d158ca8aeab8fb16a4f82de46e593c7da914
DIR\ivan.c.contreras <ivan11926@hotmail.com>
2021-08-11 14:25:32 -0500

    Se agrego ejercicio de Collections

---
+++ PrácticasExtras/OwnExercises/Collections/src/EjemploHashSet.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class EjemploHashSet {
+
+    public static void main (String[] args)
+    {
+        HashSet<String> s1 = new HashSet <String>();
+        s1.add("Hola");
+        s1.add("Hola amigo");
+        s1.add("Encantado de conocerte");
+        s1.add("Se me hace tarde");
+        s1.add("Espero verte pronto");
+        s1.add("Adios");
+
+        s1.add("Adios"); //Intentamos añadir un duplicado.
+
+        System.out.println("Numero de elementos: " + s1.size() + "\n");
+
+        Iterator i = s1.iterator();
+        while (i.hasNext()){
+            System.out.println(i.next());
+        }
+    }
+}
